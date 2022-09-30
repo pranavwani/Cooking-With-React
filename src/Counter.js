eda6444d955ccaf5f2c1d2fa67120f1cba7744e2
Pranav <pranav.eid@gmail.com>
2022-09-30 14:42:44 +0530

    First Commit

---
+++ src/Counter.js
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+import { ThemeContext } from './App';
+
+export default class Counter extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      count: props.initialCount,
+    };
+  }
+
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {(style) => (
+          <div>
+            <button style={style} onClick={() => this.changeCount(-1)}>
+              -
+            </button>
+            <span>{this.state.count}</span>
+            <button style={style} onClick={() => this.changeCount(+1)}>
+              +
+            </button>
+          </div>
+        )}
+      </ThemeContext.Consumer>
+    );
+  }
+
+  changeCount(amount) {
+    this.setState((prevState) => ({ count: prevState.count + amount }));
+  }
+}
